0e446efd87a8355d4c41a1fae23f1613383dd321
Quoc Khanh Khong <khanhquoc1125@gmail.com>
2025-06-02 10:21:59 +0700

    refactor(name): Rename variables and method for descriptive names

--- M10/maxflow.cpp
+++ M10/maxflow.cpp
@@ -24,142 +24,136 @@ using namespace std;
 #pragma GCC optimize("O2")
 #pragma GCC target("avx,avx2,fma")
 const int mod = 1e9 + 7;
-const int inf = 1e9 + 7;
-const int N = 1e6 + 7 ;
-struct edge{
-	int to;
-	int capacity;
-	int flow;
-	int id;
+const int MAX_CAPACITY = 1e9 + 7;
+const int MAX_NODES = 1e6 + 7;
+
+struct Edge {
+    int destination;
+    int capacity;
+    int flow;
+    int reverseEdgeIndex;
 };
 
-struct Dinic{
-	int inf;
-	int numbVertex;
-	vector<vector<edge>> adj;
-	vector<int> level;
-	int source, sink;
-	
-	
-	Dinic(int _numbVertex, int _source, int _sink, int _inf ){
-		numbVertex = _numbVertex;
-		source = _source;
-		sink = _sink;
-		inf = _inf;
-		adj.clear();
-		level.clear();
-		adj.resize(numbVertex);
-		level.resize(numbVertex);
-	}
-	
-	void addEdge(int u, int v, int weight){
-		int id_u = (int)adj[u].size();
-		int id_v = (int)adj[v].size();
-		
-		adj[u].push_back((edge){
-			v, weight, 0, id_u
-		});
-		
-		adj[v].push_back((edge){
-			u, 0, 0, id_v
-		});
-	}
-	
-	bool bfs(){
-		for(int i = 0; i < numbVertex; i++) level[i] = -1;
-		
-		queue<int> myqueue;
-		myqueue.push(source);
-		
-		level[source] = 0;
-		
-		while(!myqueue.empty()){
-			int current = myqueue.front();
-			cout << "Loop here" << endl;
-			for(int i = 0; i < (int)adj[current].size(); i++){
+struct DinicMaxFlow {
+    int maxCapacity;
+    int numNodes;
+    vector<vector<Edge>> adjacencyList;
+    vector<int> distance;
+    int sourceNode, sinkNode;
 
-				edge neighbor = adj[current][i];
-				if(neighbor.flow < neighbor.capacity && level[neighbor.to] == -1){
-					level[neighbor.to] = level[current] + 1;
-					myqueue.push(neighbor.to);
-				}
-			}
-		}
-		
-		return (level[sink] != -1);
-	}
-	
-	int sendFlow(int u, int sink, int currentFlow, vector<int> &start){
-		if(u == sink) return currentFlow;
-		
-		for(; start[u] < (int)adj[u].size(); start[u]++){
-			edge &neighbor = adj[u][start[u]];
-			
-			if(level[neighbor.to] == level[u] + 1 && neighbor.flow < neighbor.capacity){
-				int flow = sendFlow(neighbor.to, sink, min(currentFlow, neighbor.capacity - neighbor.flow), start);
-				if(flow > 0){
-					neighbor.flow += flow;
-					edge &reverseEdge = adj[neighbor.to][neighbor.id];
-					reverseEdge.flow -= flow;
-					return flow;
-				}
-				
-			}
-		}
-		return 0;
-	} 
-	
-	int dinicMaxFlow(){
-		int sum = 0LL;
-		while(!bfs()){
-			vector<int> start(numbVertex, 0);
-			int flow = sendFlow(source, sink, inf, start);
-			if(flow == 0) break;
-			sum += flow;
-		}
-		return sum;
-	}
+    DinicMaxFlow(int nodeCount, int source, int sink, int maxCap) {
+        numNodes = nodeCount;
+        sourceNode = source;
+        sinkNode = sink;
+        maxCapacity = maxCap;
+        adjacencyList.clear();
+        distance.clear();
+        adjacencyList.resize(numNodes);
+        distance.resize(numNodes);
+    }
+
+    void addDirectedEdge(int from, int to, int edgeCapacity) {
+        int fromEdgeIndex = (int)adjacencyList[from].size();
+        int toEdgeIndex = (int)adjacencyList[to].size();
+
+        adjacencyList[from].push_back((Edge){
+            to, edgeCapacity, 0, toEdgeIndex
+        });
+
+        adjacencyList[to].push_back((Edge){
+            from, 0, 0, fromEdgeIndex
+        });
+    }
+
+    bool buildLevelGraph() {
+        for(int i = 0; i < numNodes; i++) distance[i] = -1;
+
+        queue<int> nodeQueue;
+        nodeQueue.push(sourceNode);
+        distance[sourceNode] = 0;
+
+        while(!nodeQueue.empty()) {
+            int currentNode = nodeQueue.front();
+            nodeQueue.pop();
+            for(int i = 0; i < (int)adjacencyList[currentNode].size(); i++) {
+                Edge neighbor = adjacencyList[currentNode][i];
+                if(neighbor.flow < neighbor.capacity && distance[neighbor.destination] == -1) {
+                    distance[neighbor.destination] = distance[currentNode] + 1;
+                    nodeQueue.push(neighbor.destination);
+                }
+            }
+        }
+
+        return (distance[sinkNode] != -1);
+    }
+
+    int augmentPath(int currentNode, int targetSink, int currentFlow, vector<int> &nextEdgeIndex) {
+        if(currentNode == targetSink) return currentFlow;
+
+        for(; nextEdgeIndex[currentNode] < (int)adjacencyList[currentNode].size(); nextEdgeIndex[currentNode]++) {
+            Edge &neighbor = adjacencyList[currentNode][nextEdgeIndex[currentNode]];
+
+            if(distance[neighbor.destination] == distance[currentNode] + 1 && neighbor.flow < neighbor.capacity) {
+                int flow = augmentPath(neighbor.destination, targetSink, min(currentFlow, neighbor.capacity - neighbor.flow), nextEdgeIndex);
+                if(flow > 0) {
+                    neighbor.flow += flow;
+                    Edge &reverseEdge = adjacencyList[neighbor.destination][neighbor.reverseEdgeIndex];
+                    reverseEdge.flow -= flow;
+                    return flow;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int computeMaxFlow() {
+        int totalFlow = 0;
+        while(buildLevelGraph()) {
+            while(true) {
+                vector<int> nextEdgeIndex(numNodes, 0);
+                int flow = augmentPath(sourceNode, sinkNode, maxCapacity, nextEdgeIndex);
+                if(flow == 0) break;
+                totalFlow += flow;
+            }
+        }
+        return totalFlow;
+    }
 };
 
-int n, m, source, sink;
+int numNodes, numEdges, sourceNode, sinkNode;
 
-int main(){
-	ios :: sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-	freopen(TASK".inp", "r", stdin);
-//	freopen(TASK".out", "w", stdout);
-	
-	cin>>n>>m;
-	source = 1;
-	sink = n;
-	Dinic dinic(n + 5, source, sink, 1e9 + 7);
-	
-	for(int i = 1; i <= m; i++){
-		int u, v, weight;	
-		cin>>u>>v>>weight;
-		dinic.addEdge(u, v, weight);	
-	}
-	
-	int maxFlow = dinic.dinicMaxFlow();
-	
-	vector<tuple<int, int, int>> listEdges;
-	
-	for(int u = 0; u < n; u++){
-		for(int i = 0; i < (int)dinic.adj[u].size(); i++){
-			edge &e = dinic.adj[u][i];
-			
-			if(e.flow > 0){
-				listEdges.push_back(make_tuple(u, e.to, e.flow));
-			}
-		}
-	}
-	
-	cout << maxFlow;
+    freopen(TASK".inp", "r", stdin);
 
+    cin >> numNodes >> numEdges;
+    sourceNode = 1;
+    sinkNode = numNodes;
+    DinicMaxFlow dinic(numNodes + 5, sourceNode, sinkNode, MAX_CAPACITY);
 
+    for(int i = 1; i <= numEdges; i++) {
+        int fromNode, toNode, edgeCapacity;
+        cin >> fromNode >> toNode >> edgeCapacity;
+        dinic.addDirectedEdge(fromNode, toNode, edgeCapacity);
+    }
 
-	return 0;
+    int maxFlow = dinic.computeMaxFlow();
+
+    vector<tuple<int, int, int>> usedEdges;
+
+    for(int node = 0; node < numNodes; node++) {
+        for(int i = 0; i < (int)dinic.adjacencyList[node].size(); i++) {
+            Edge &edge = dinic.adjacencyList[node][i];
+
+            if(edge.flow > 0) {
+                usedEdges.push_back(make_tuple(node, edge.destination, edge.flow));
+            }
+        }
+    }
+
+    cout << maxFlow;
+
+    return 0;
 }
-
-
